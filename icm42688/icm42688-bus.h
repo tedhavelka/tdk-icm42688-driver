97902e6d277f682df20855ff3d05559f573eb530
Theodore Havelka <tedhavelka66@gmail.com>
2026-06-26 07:08:26 +0000

    driver: refactor general bus read and write functions to allow for Zephyr
    logging use.

--- icm42688/icm42688-bus.h
+++ icm42688/icm42688-bus.h
@@ -13,99 +13,14 @@
 
 #define BYTE_COUNT_ONE 1
 
-static inline int icm42688_bus_read(const struct device *dev,
+int icm42688_bus_read(const struct device *dev,
 				    uint8_t reg,
 				    uint8_t *buf,
-				    uint16_t len)
-{
-	struct icm42688_dev_data *data = dev->data;
-	// TODO [x] check 'struct icm42688_dev_data' has a member named 'rtio':
-	struct rtio *ctx = data->ctx;
-	struct rtio_iodev *iodev = data->iodev;
-	// RTIO submission and completion queue constructs:
-	struct rtio_sqe *write_sqe = rtio_sqe_acquire(ctx);
-	struct rtio_sqe *read_sqe = rtio_sqe_acquire(ctx);
-	struct rtio_cqe *cqe;
+				    uint16_t len);
 
-	int rc = 0;
-
-	if (!write_sqe || !read_sqe) {
-		return -ENOMEM;
-	}
-
-	// TODO [ ] Check whether REG_READ_BIT defined:
-	reg = reg | REG_READ_BIT;
-
-	// TODO [ ] Learn what the parameters 5 and 6 of API rtio_sqe_prep_write() represents:
-	rtio_sqe_prep_write(write_sqe, iodev, RTIO_PRIO_HIGH, &reg, 1, NULL);
-	write_sqe->flags |= RTIO_SQE_TRANSACTION;
-	rtio_sqe_prep_read(read_sqe, iodev, RTIO_PRIO_HIGH, buf, len, NULL);
-	if (data->type == ICM42688_BUS_I2C) {
-		read_sqe->iodev_flags |= RTIO_IODEV_I2C_STOP | RTIO_IODEV_I2C_RESTART;
-	}
-
-	// TODO [ ] Learn what the second parameter of API rtio_submit() represents:
-	rc = rtio_submit(ctx, 2); 
-	if (rc) {
-		return rc;
-	}
-
-	do {
-		cqe = rtio_cqe_consume(ctx);
-		if (cqe != NULL) {
-			rc = cqe->result;
-			rtio_cqe_release(ctx, cqe);
-		}
-	} while (cqe != NULL);
-
-	return rc;
-}
-
-static inline int icm42688_bus_write(const struct device *dev,
+int icm42688_bus_write(const struct device *dev,
 				     uint8_t reg,
 				     const uint8_t *buf,
-				     uint16_t len)
-{
-	struct icm42688_dev_data *data = dev->data;
-	struct rtio *ctx = data->ctx;
-	struct rtio_iodev *iodev = data->iodev;
-	// RTIO submission and completion queue constructs:
-	struct rtio_sqe *write_reg_sqe = rtio_sqe_acquire(ctx);
-	struct rtio_sqe *write_buf_sqe = rtio_sqe_acquire(ctx);
-	struct rtio_cqe *cqe;
-
-	int rc;
-	LOG_INF("W1");
-
-	if (!write_reg_sqe || !write_buf_sqe) {
-		return -ENOMEM;
-	}
-	LOG_INF("W2");
-
-	rtio_sqe_prep_write(write_reg_sqe, iodev, RTIO_PRIO_HIGH, &reg, 1, NULL);
-	write_reg_sqe->flags |= RTIO_SQE_TRANSACTION;
-	rtio_sqe_prep_write(write_buf_sqe, iodev, RTIO_PRIO_HIGH, buf, len, NULL);
-	if (data->type == ICM42688_BUS_I2C) {
-		write_buf_sqe->iodev_flags |= RTIO_IODEV_I2C_STOP;
-	}
-	LOG_INF("W3");
-
-	rc = rtio_submit(ctx, 2);
-	if (rc) {
-		return rc;
-	}
-	LOG_INF("W4");
-
-	do {
-		cqe = rtio_cqe_consume(ctx);
-		if (cqe != NULL) {
-			rc = cqe->result;
-			rtio_cqe_release(ctx, cqe);
-		}
-	} while (cqe != NULL);
-	LOG_INF("W5");
-
-	return rc;
-}
+				     uint16_t len);
 
 #endif // ORESAT_DRIVER_SENSOR_ICM42688_BUS_H
